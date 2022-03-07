4648a1f718dc3331df1420c3af10fff48b9aa687
seojiwon0702 <78357979+seojiwon0702@users.noreply.github.com>
2022-03-07 17:38:54 +0900

    Update axios.js

--- frontend/fitmate/src/module/axios.js
+++ frontend/fitmate/src/module/axios.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  // baseURL: "https://localhost:8443",
-  baseURL: "https://192.168.0.5:8443",
+  baseURL: "https://localhost:8443",
 });
